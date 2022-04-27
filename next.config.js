8c39150f6c2743d5722c5084781fe4f35dafc0ac
Nagy Botond <botondnagy98@icloud.com>
2022-04-27 16:03:21 +0300

    added security headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,53 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';  
+`;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	poweredByHeader: false,
+	generateEtags: false,
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: [
+					{
+						key: "X-DNS-Prefetch-Control",
+						value: "on",
+					},
+					{
+						key: "Strict-Transport-Security",
+						value: "max-age=31536000; includeSubDomains; preload",
+					},
+					{
+						key: "Content-Security-Policy",
+						value: ContentSecurityPolicy.replace(/\s{2,}/g, "").trim(),
+					},
+					{
+						key: "X-Content-Type-Options",
+						value: "nosniff",
+					},
+					{
+						key: "X-Frame-Options",
+						value: "SAMEORIGIN",
+					},
+					{
+						key: "Referrer-Policy",
+						value: "origin-when-cross-origin",
+					},
+					{
+						key: "Permissions-Policy",
+						value: "none",
+					},
+				],
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
